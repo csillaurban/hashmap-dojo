c0cb762cf93e0c791b13d022395b47935b660e85
Csilla Urban <csillaurban@gmail.com>
2019-05-01 18:39:07 +0200

    create interface and skeleton methods in Hashmap class

--- src/hashmap/HashMap.java
+++ src/hashmap/HashMap.java
@@ -1,4 +1,33 @@
 package hashmap;
 
-public class HashMap {
+public class HashMap implements HashMapInterface{
+    private int bucket = 16;
+
+    @Override
+    public void add(String key, int value) {
+
+    }
+
+    @Override
+    public int getValue(String key) {
+        return 0;
+    }
+
+    @Override
+    public void remove(String key) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
+
+    private Integer getHash(String key) {
+        return null;
+    }
+
+    private void resizeIfNeeded() {
+
+    }
 }
---
+++ src/hashmap/HashMapInterface.java
@@ -0,0 +1,12 @@
+package hashmap;
+
+public interface HashMapInterface {
+
+    public void add(String key, int value);
+
+    public int getValue(String key);
+
+    public void remove(String key);
+
+    public void clearAll();
+}
